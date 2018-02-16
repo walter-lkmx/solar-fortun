a06634c627b8a26e204f47e6406f4623f529b2c7
edith moreno <edith@Dianas-Mac-mini.local>
2018-02-15 17:16:52 -0800

    Style settings.

--- index.php
+++ index.php
@@ -4,30 +4,30 @@
 <section class="main-container">
   <?php include('ui-layout/top-bar.php'); ?>
   <section class="content" id="h-scroll">
-    <nav class="products" >
+    <nav class="products">
       <ul>
         <li>
-          <a href="" title="">
+          <a href="/noble-cru" title="">
             <img src="<?php bloginfo('template_url'); ?>/img/bottles/SF_Noble.png">
           </a>
         </li>
         <li>
-          <a href="" title="">
+          <a href="/o-positivo" title="">
             <img src="<?php bloginfo('template_url'); ?>/img/bottles/SF_OPositivo.png">
           </a>
         </li>
         <li>
-          <a href="" title="">
+          <a href="/confabulario" title="">
             <img src="<?php bloginfo('template_url'); ?>/img/bottles/SF_Confabulario.png">
           </a>
         </li>        
         <li class="">
-          <a href="" title="">
+          <a href="/vina-rosa" title="">
             <img src="<?php bloginfo('template_url'); ?>/img/bottles/SF_VinaRosa.png">
           </a>
         </li>
         <li class="">
-          <a href="" title="">
+          <a href="/elixir-cordis" title="">
             <img src="<?php bloginfo('template_url'); ?>/img/bottles/SF_Elixir.png">
           </a>
         </li>
