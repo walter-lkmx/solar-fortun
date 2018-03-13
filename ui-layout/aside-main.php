a7605ee526485d6608a2e82a738f2d9cb829d0b6
edith moreno <edith@Dianas-Mac-mini.local>
2018-03-12 17:07:13 -0700

    Navbar settings: fix to stain on hover.

--- ui-layout/aside-main.php
+++ ui-layout/aside-main.php
@@ -11,49 +11,65 @@
       <li>
         <a href="" title="">
         <span>Tienda</span>
-        <img src="<?php bloginfo('template_url'); ?>/img/brush.svg">
+        <div class="stain">
+          <img src="<?php bloginfo('template_url'); ?>/img/brush.svg">
+        </div>
       </a>
       </li>
       <li class="spacer">
         <a href="" title="">
           <span>Romance</span>
-          <img src="<?php bloginfo('template_url'); ?>/img/brush.svg">
+          <div class="stain">
+            <img src="<?php bloginfo('template_url'); ?>/img/brush.svg">
+          </div>
         </a>
       </li>
       <li>
         <a href="" title="">
           <span>Noticias</span>
-          <img src="<?php bloginfo('template_url'); ?>/img/brush.svg">
+          <div class="stain">
+            <img src="<?php bloginfo('template_url'); ?>/img/brush.svg">
+          </div>
         </a>
         </li>
       <li>
         <a href="" title="">
           <span>Nosotros</span>
-          <img src="<?php bloginfo('template_url'); ?>/img/brush.svg">
+          <div class="stain">
+            <img src="<?php bloginfo('template_url'); ?>/img/brush.svg">
+          </div>
         </a>
       </li>
       <li class="spacer">
         <a href="" title="">
           <span>Enólogo</span>
-          <img src="<?php bloginfo('template_url'); ?>/img/brush.svg">
+          <div class="stain">
+            <img src="<?php bloginfo('template_url'); ?>/img/brush.svg">
+          </div>
         </a>
       </li>
       <li>
         <a href="" title="">
           <span>Privacidad</span>
-          <img src="<?php bloginfo('template_url'); ?>/img/brush.svg">
+          <div class="stain">
+            <img src="<?php bloginfo('template_url'); ?>/img/brush.svg">
+          </div>
         </a>
       </li>
       <li>
         <a href="" title="">
           <span>Ubicación</span>
-          <img src="<?php bloginfo('template_url'); ?>/img/brush.svg">
+          <div class="stain">
+            <img src="<?php bloginfo('template_url'); ?>/img/brush.svg">
+          </div>
         </a>
       </li>
       <li>
         <a href="" title="">
           <span>Contacto</span>
-          <img src="<?php bloginfo('template_url'); ?>/img/brush.svg">
+          <div class="stain">
+            <img src="<?php bloginfo('template_url'); ?>/img/brush.svg">
+          </div>
         </a>
       </li>
     </ul>
