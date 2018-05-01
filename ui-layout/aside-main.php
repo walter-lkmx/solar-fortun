0d923e8b6fdbd29461c9c0535725930d52e45a39
edith moreno <edith@Dianas-Mac-mini.local>
2018-04-30 17:02:21 -0700

    Style settings (unfinished…)

--- ui-layout/aside-main.php
+++ ui-layout/aside-main.php
@@ -16,7 +16,7 @@
         </div>
       </a>
       </li>
-      <li class="spacer">
+      <li>
         <a href="" title="">
           <span class="jarallax" data-jarallax-element="-80">Romance</span>
           <div class="stain">
@@ -24,7 +24,7 @@
           </div>
         </a>
       </li>
-      <li>
+      <li class="spacer">
         <a href="/noticias" title="">
           <span>Noticias</span>
           <div class="stain">
@@ -33,15 +33,23 @@
         </a>
         </li>
       <li>
-        <a href="" title="">
+        <a href="/nosotros" title="">
           <span>Nosotros</span>
           <div class="stain">
             <img src="<?php bloginfo('template_url'); ?>/img/brush.svg">
           </div>
         </a>
       </li>
+      <li>
+        <a href="/fundador" title="">
+          <span>Fundador</span>
+          <div class="stain">
+            <img src="<?php bloginfo('template_url'); ?>/img/brush.svg">
+          </div>
+        </a>
+      </li>
       <li class="spacer">
-        <a href="" title="">
+        <a href="/enologo" title="">
           <span>Enólogo</span>
           <div class="stain">
             <img src="<?php bloginfo('template_url'); ?>/img/brush.svg">
@@ -49,7 +57,7 @@
         </a>
       </li>
       <li>
-        <a href="" title="">
+        <a href="/privacidad" title="">
           <span>Privacidad</span>
           <div class="stain">
             <img src="<?php bloginfo('template_url'); ?>/img/brush.svg">
@@ -57,7 +65,7 @@
         </a>
       </li>
       <li>
-        <a href="" title="">
+        <a href="/ubicacion" title="">
           <span>Ubicación</span>
           <div class="stain">
             <img src="<?php bloginfo('template_url'); ?>/img/brush.svg">
@@ -65,7 +73,7 @@
         </a>
       </li>
       <li>
-        <a href="" title="">
+        <a href="/contacto" title="">
           <span>Contacto</span>
           <div class="stain">
             <img src="<?php bloginfo('template_url'); ?>/img/brush.svg">
