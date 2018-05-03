a061fdfff873b50a619ee1606951975dc6095be0
walter hurtado <walter.hurtado@lkmx.io>
2018-05-03 14:58:43 -0700

    custom map - API Mapbox

--- contact.php
+++ contact.php
@@ -6,7 +6,39 @@
   <section class="content" id="v-scroll">
     <section class="single">
       <div class="featured-image">
-        <?php the_post_thumbnail() ?>
+      <div id='map' style='width: 100%; height: 300vh;'></div>
+        <script>
+          mapboxgl.accessToken = 'pk.eyJ1Ijoid2FsdGVyaGYiLCJhIjoiY2l5NHB6cjZvMDAydzJ3cXA2OGlrZnNjNSJ9.bZB809FBgbV63mMS8dhseg';
+          var map = new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/walterhf/cjgr0abr200062rpbyjlm5mcg',
+          zoom:8.5,
+          center: ["-116.5727153","30.9000000"]
+          });
+
+          map.on("load", function () {
+            /* Image: An image is loaded and added to the map. */
+            map.loadImage( templateDir + "/img/solar-pin.png", function(error, image) {
+                if (error) throw error;
+                map.addImage("custom-marker", image);
+                /* Style layer: A style layer ties together the source and image and specifies how they are displayed on the map. */
+                map.addLayer({
+                  id: "markers",
+                  type: "symbol",
+                  /* Source: A data source specifies the geographic coordinate where the image marker gets placed. */
+                  source: {
+                    type: "geojson",
+                    data: {
+                      type: "FeatureCollection",
+                      features:[{"type":"Feature","geometry":{"type":"Point","coordinates":["-116.5727153","32.1124706"]}}]}
+                  },
+                  layout: {
+                    "icon-image": "custom-marker",
+                  }
+                });
+              });
+          });
+        </script>
       </div>
       <div class="content"> 
         <section class="title">
--- header.php
+++ header.php
@@ -4,7 +4,9 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-		<?php wp_head(); ?> 
+    <?php wp_head(); ?> 
+    <script src='https://api.mapbox.com/mapbox-gl-js/v0.44.2/mapbox-gl.js'></script>
+    <link href='https://api.mapbox.com/mapbox-gl-js/v0.44.2/mapbox-gl.css' rel='stylesheet' />
 	</head>
   <body <?php body_class(); ?>>
   <script type="text/javascript">
