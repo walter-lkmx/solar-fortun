497f3e2ed7e6938cf21fffb7e6c13bddf0ea8fdb
walter hurtado <walter.hurtado@lkmx.io>
2018-05-15 10:16:01 -0700

    fancy stuff for android (^lollipop) nabber

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#232734" />
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
     <?php wp_head(); ?> 
     <script src='https://api.mapbox.com/mapbox-gl-js/v0.44.2/mapbox-gl.js'></script>
