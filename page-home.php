3390e85eb9ca71cfa39d8bd682f691985d6cdca2
edith moreno <edith@Dianas-Mac-mini.local>
2018-07-17 12:42:53 -0700

    Bottle labels customized: link to the product. Black button changed to ‘Add to cart’, with respective link.

--- page-home.php
+++ page-home.php
@@ -12,13 +12,17 @@
             <img class="bottle-mixtures" src="<?php bloginfo('template_url'); ?>/img/bottles/confabulario.png">
           </a>
           <img class="seal noble-cru" src="<?php bloginfo('template_url'); ?>/img/seals/_seal-confabulario.png">
-          <section class="title">
-            <h1 class="confabulario">Confa~<br>bulario</h1>
-            <div class="add-cart">
-              <span>Ver detalles</span>
-              <!-- <div class="img-container"><img src="<?php bloginfo('template_url'); ?>/img/cart.svg"></div> -->
-            </div>
-          </section>
+          <a href="catalogo/confabulario">
+            <section class="title">
+              <h1 class="confabulario">Confa~<br>bulario</h1>
+              <a href="/catalogo/?add-to-cart=1564">
+                <div class="add-cart">
+                  <span>Añadir a carrito</span>
+                  <!-- <div class="img-container"><img src="<?php bloginfo('template_url'); ?>/img/cart.svg"></div> -->
+                </div>
+              </a>
+            </section>
+          </a>
           <div class="stain">
             <img class="confabulario" src="<?php bloginfo('template_url'); ?>/img/watercolors/_SolarFortun_Confabulario_fondo.png">
           </div>
@@ -29,13 +33,17 @@
             <img class="bottle-mixtures" src="<?php bloginfo('template_url'); ?>/img/bottles/opositivo.png">
           </a>
           <img class="seal noble-cru" src="<?php bloginfo('template_url'); ?>/img/seals/_seal-opositivo.png">
-          <section class="title">
-            <h1 class="o-positivo">O Positivo</h1>
-            <div class="add-cart">
-              <span>Ver detalles</span>
-              <!-- <div class="img-container"><img src="<?php bloginfo('template_url'); ?>/img/cart.svg"></div> -->
-            </div>
-          </section>
+          <a href="catalogo/o-positivo">
+            <section class="title">
+              <h1 class="o-positivo">O Positivo</h1>
+              <a href="/catalogo/?add-to-cart=1565">
+                <div class="add-cart">
+                  <span>Añadir a carrito</span>
+                  <!-- <div class="img-container"><img src="<?php bloginfo('template_url'); ?>/img/cart.svg"></div> -->
+                </div>
+              </a>
+            </section>
+          </a>
           <div class="stain">
             <img class="positivo" src="<?php bloginfo('template_url'); ?>/img/watercolors/_SolarFortun_OPositivo_fondo.png">
           </div>
@@ -46,13 +54,17 @@
             <img class="bottle-mixtures" src="<?php bloginfo('template_url'); ?>/img/bottles/noblecru.png">
           </a>
           <img class="seal noble-cru" src="<?php bloginfo('template_url'); ?>/img/seals/_seal-noble.png">
-          <section class="title">
-            <h1 class="noble-cru">Noble Cru</h1>
-            <div class="add-cart">
-              <span>Ver detalles</span>
-              <!-- <div class="img-container"><img src="<?php bloginfo('template_url'); ?>/img/cart.svg"></div> -->
-            </div>
-          </section>
+          <a href="catalogo/noble-cru">
+            <section class="title">
+              <h1 class="noble-cru">Noble Cru</h1>
+              <a href="/catalogo/?add-to-cart=1566">
+                <div class="add-cart">
+                  <span>Añadir a carrito</span>
+                  <!-- <div class="img-container"><img src="<?php bloginfo('template_url'); ?>/img/cart.svg"></div> -->
+                </div>
+              </a>
+            </section>
+          </a>
           <div class="stain">
             <img class="noble-cru" src="<?php bloginfo('template_url'); ?>/img/watercolors/_SolarFortun_NobelCru_fondo.png">
           </div>
@@ -63,13 +75,17 @@
             <img class="bottle-vina-rosa" src="<?php bloginfo('template_url'); ?>/img/bottles/vinaenrosa.png">
           </a>
           <img class="seal noble-cru" src="<?php bloginfo('template_url'); ?>/img/seals/_seal-vinarosa.png">
-          <section class="title">
-            <h1 class="vina-rosa">La Viña<br> en Rosa</h1>
-            <div class="add-cart">
-              <span>Ver detalles</span>
-              <!-- <div class="img-container"><img src="<?php bloginfo('template_url'); ?>/img/cart.svg"></div> -->
-            </div>
-          </section>
+          <a href="catalogo/la-vina-en-rosa">
+            <section class="title">
+              <h1 class="vina-rosa">La Viña<br> en Rosa</h1>
+              <a href="/catalogo/?add-to-cart=1562">
+                <div class="add-cart">
+                  <span>Añadir a carrito</span>
+                  <!-- <div class="img-container"><img src="<?php bloginfo('template_url'); ?>/img/cart.svg"></div> -->
+                </div>
+              </a>
+            </section>
+          </a>
           <div class="stain">
             <img class="vina-rosa" src="<?php bloginfo('template_url'); ?>/img/watercolors/_SolarFortun_EnRosa_fondo.png">
           </div>
@@ -80,13 +96,17 @@
             <img class="bottle-elixir-cordis" src="<?php bloginfo('template_url'); ?>/img/bottles/elixircordis.png">
           </a>
           <img class="seal noble-cru" src="<?php bloginfo('template_url'); ?>/img/seals/_seal-elixir.png">
-          <section class="title">
-            <h1 class="elixir-cordis">Elixir<br> Cordis</h1>
-            <div class="add-cart">
-              <span>Ver detalles</span>
-              <!-- <div class="img-container"><img src="<?php bloginfo('template_url'); ?>/img/cart.svg"></div> -->
-            </div>
-          </section>
+          <a href="catalogo/elixir-cordis">
+            <section class="title">
+              <h1 class="elixir-cordis">Elixir<br> Cordis</h1>
+              <a href="/catalogo/?add-to-cart=1563">
+                <div class="add-cart">
+                  <span>Añadir a carrito</span>
+                  <!-- <div class="img-container"><img src="<?php bloginfo('template_url'); ?>/img/cart.svg"></div> -->
+                </div>
+              </a>
+            </section>
+          </a>
           <div class="stain">
             <img class="elixir" src="<?php bloginfo('template_url'); ?>/img/watercolors/_SolarFortun_Elixir_fondo.png">
           </div>
