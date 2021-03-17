3d874e70643940cb2a4b0865698c57b211db78e6
edith moreno <edith.moreno@lkmx.io>
2021-03-17 09:06:29 -0700

    Alert message implemented if User Agent is Firefox. New icon added on assets.

--- ui-layout/top-bar.php
+++ ui-layout/top-bar.php
@@ -1,4 +1,38 @@
 <?php include('mini-cart.php'); ?>
+<?php
+  $arr_browsers = ["Opera", "Edg", "Chrome", "Safari", "Firefox", "MSIE", "Trident"];
+  
+  $agent = $_SERVER['HTTP_USER_AGENT'];
+  
+  $user_browser = '';
+  foreach ($arr_browsers as $browser) {
+      if (strpos($agent, $browser) !== false) {
+          $user_browser = $browser;
+          break;
+      }   
+  }
+    
+  switch ($user_browser) {
+      case 'MSIE':
+          $user_browser = 'Internet Explorer';
+          break;
+    
+      case 'Trident':
+          $user_browser = 'Internet Explorer';
+          break;
+    
+      case 'Edg':
+          $user_browser = 'Microsoft Edge';
+          break;
+  }
+
+  if (($user_browser == 'Firefox') && ($cart_item > 0)) {
+    $bloginfo = get_template_directory_uri();
+    echo "<section class='alert'>
+    <p>Para una mejor experiencia, te recomendamos hacer tu compra mediante Google Chrome <img class='alert_icon' src='{$bloginfo}/img/solar-chrome.svg'></p>
+  </section>";
+  }
+?>
 <section class="top-bar" id="header">
   <a class="mobile-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php bloginfo('template_url'); ?>/img/solar-logo-text.svg"></a>
   <nav>
