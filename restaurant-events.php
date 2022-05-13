af9cb4db375f46fbd68e748a7ee76f51a2391926
Gerardo Ibarra <luis_astec2@hotmail.com>
2022-05-13 14:38:52 -0700

    Added Diner's Choice insignia in restaurant page

--- restaurant-events.php
+++ restaurant-events.php
@@ -3,17 +3,46 @@
 <?php include('ui-layout/aside-main.php'); ?>
 <section class="main-container">
   <?php include('ui-layout/top-bar.php'); ?>
+  <style>
+    .diners-choice-insignia-container {
+      position: absolute;
+      width: 100%;
+    }
+
+    .diners-choice-insignia {
+      position: absolute;
+      width: 180px;
+      height: 180px;
+      right: -25px;
+      top: 30px;
+      background: url("https://restaurant.opentable.com.mx/badge/ot/DC-2021.png") no-repeat;
+      background-size: contain;
+    }
+
+    @media only screen and (max-width: 919px) {
+      .diners-choice-insignia {
+        width: 120px;
+        height: 120px;
+        right: -25px;
+        top: 0px;
+      }
+    }
+  </style>
   <section class="page-template" id="v-scroll">
     <section class="single">
       <div class="featured-image oenologist-img">
         <?php the_post_thumbnail() ?>
       </div>
-      <div class="content"> 
+
+      <a href="http://www.opentable.com.mx/restaurant/profile/1120630/reserve?rid=1120630&restref=1120630" target="_blank" rel="noopener">
+        <div class="diners-choice-insignia"></div>
+      </a>
+      <div class="content">
         <section class="title">
-          <h1><?php the_title() ?></h1> 
+          <h1><?php the_title() ?></h1>
         </section>
         <div class="text">
-			    <?php the_content() ?>
+          <?php the_content() ?>
         </div>
       </div>
     </section>
