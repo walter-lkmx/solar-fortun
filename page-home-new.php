5bd403cbdbdfb8d085b43c056af2c49589dc35f2
Gerardo Ibarra <luis_astec2@hotmail.com>
2022-05-19 20:53:36 -0700

    redesign done NO instagram posts yet

--- page-home-new.php
+++ page-home-new.php
@@ -43,7 +43,7 @@ function woo_featured_products()
     ?>
 
       <li class="featured">
-        <div class="featured-watercolor" style="background: url('/wp-content/themes/solar-v2/<?php echo $watercolor ?>') no-repeat center ; background-size: 40%;">
+        <div class="featured-watercolor" style="background: url('<?php echo bloginfo('template_url') . '/' . $watercolor ?>') no-repeat center ; background-size: 40%;">
           <a href="<?php echo get_permalink($loop->post->ID); ?>">
             <?php the_post_thumbnail('medium'); ?>
           </a>
@@ -64,6 +64,49 @@ function woo_featured_products()
   return ob_get_clean();
 }
 
+function wpb_postsbycategory()
+{
+  // the query
+  $the_query = new WP_Query(array(
+    'category_name' => 'noticia',
+    'posts_per_page' => 2
+  ));
+
+  // The Loop
+  if ($the_query->have_posts()) {
+    $string .= '<ul class="postsbycategory widget_recent_entries">';
+    while ($the_query->have_posts()) {
+      $the_query->the_post();
+      if (has_post_thumbnail()) {
+        $string .= '<li class="featured-news">';
+        $string .= get_the_post_thumbnail($post_id, array(160, 236));
+        $string .= '<div class="news-title-container"><a href="' . get_the_permalink() . '" rel="bookmark">' . get_the_title() . '</a></div>';
+
+        $content = get_the_content();
+        $content = str_replace(['<!-- wp:paragraph -->', '<!-- /wp:paragraph -->'], '', $content);
+        $content = trim(substr($content, 0, 100));
+
+
+        $string .= '<div class="news-content-container"><p>' . $content . '...</p></div>'; #Post content
+        $string .= '</li>';
+      } else {
+        // if no featured image is found
+        $string .= '<li><a href="' . get_the_permalink() . '" rel="bookmark">' . get_the_title() . '</a></li>';
+      }
+    }
+  } else {
+    // no posts found
+    $string .= '<li>No Posts Found</li>';
+  }
+  $string .= '</ul>';
+
+  return $string;
+
+  /* Restore original Post Data */
+  wp_reset_postdata();
+}
+// Add a shortcode
+add_shortcode('categoryposts', 'wpb_postsbycategory');
 
 ?>
 
@@ -74,6 +117,19 @@ function woo_featured_products()
     <style>
       body {
         white-space: normal !important;
+      }
+
+      .section-more>a:hover {
+        transform: translateY(-2px);
+      }
+
+      .hoverable-title {
+        cursor: pointer;
+      }
+
+      .section-title:hover,
+      .hoverable-title:hover {
+        color: #8C1835;
       }
 
       .wonderpluginslider-container,
@@ -137,6 +193,20 @@ function woo_featured_products()
       }
 
       @media screen and (max-width: 1224px) {
+
+        li.featured-news {
+          width: 100% !important;
+        }
+
+        li.featured-news img {
+          float: none !important;
+          width: 100% !important;
+        }
+
+        li.featured-news:nth-child(2) {
+          margin-left: 0 !important;
+          margin-top: 20px !important;
+        }
 
         .home-card:nth-child(2),
         .home-card:nth-child(3) {
@@ -261,6 +331,20 @@ function woo_featured_products()
           display: block !important;
           line-height: 0;
         }
+
+        li.featured-news {
+          width: 100% !important;
+        }
+
+        li.featured-news img {
+          float: none !important;
+          width: 100% !important;
+        }
+
+        li.featured-news:nth-child(2) {
+          margin-left: 0 !important;
+          margin-top: 20px !important;
+        }
       }
 
       /*921px max-width*/
@@ -270,7 +354,7 @@ function woo_featured_products()
     <section class="cards-container">
       <div class="home-card">
         <div class="card-title">
-          <h3>Tienda</h3><span class="arrow-right"><a href="catalogo/">&rarr;</a></span>
+          <h3 class="hoverable-title">Tienda</h3><span class="arrow-right"><a href="catalogo/">&rarr;</a></span>
         </div>
         <hr class="card-spacer">
         <div class="card-body"><br>Compra nuestros vinos a domicilio, con env&iacute;os a todo M&eacute;xico
@@ -279,7 +363,7 @@ function woo_featured_products()
       </div>
       <div class="home-card">
         <div class="card-title">
-          <h3>Dulce Vida</h3><span class="arrow-right"><a href="restaurante-eventos/">&rarr;</a></span>
+          <h3 class="hoverable-title">Dulce Vida</h3><span class="arrow-right"><a href="restaurante-eventos/">&rarr;</a></span>
         </div>
         <hr class="card-spacer">
         <div class="card-body"><br>Visita nuestro restaurante y disfruta de un asador campestre a cargo de la chef Dulce López. Conoce nuestros maridajes y platillos ganadores a la sombra de nuestros encinos centenarios.
@@ -288,7 +372,7 @@ function woo_featured_products()
       </div>
       <div class="home-card">
         <div class="card-title">
-          <h3>Vinícola</h3><span class="arrow-right"><a href="nosotros/">&rarr;</a></span>
+          <h3 class="hoverable-title">Vinícola</h3><span class="arrow-right"><a href="nosotros/">&rarr;</a></span>
         </div>
         <hr class="card-spacer">
         <div class="card-body"><br>Solar Fortún es una vinícola familiar mexicana ubicada en Francisco Zarco, Valle de Guadalupe. El proyecto nace en 2007 con el viñedo y a partir del 2012 iniciamos con la distribución de nuestros vinos.
@@ -298,10 +382,10 @@ function woo_featured_products()
     </section>
     <br>
     <section class="titled-section">
-      <h1 class="title">
+      <h1 class="title hoverable-title">
         Nuestra Familia
-        <span class="section-more"><a href="nosotros/">Ver más</a> </span>
       </h1>
+      <span class="section-more"><a href="nosotros/">Ver más</a> </span>
       <div class="section-body" style="max-width:100%;">
         <div class="our-family-card founder">
           <div class="upper-card" style="height:40%;"></div>
@@ -322,9 +406,68 @@ function woo_featured_products()
       </div>
     </section>
     <section class="titled-section">
-      <dvi class="section-body"></dvi>
+      <div class="section-body"></div>
     </section>
     <style>
+      ul.postsbycategory {
+        width: 100%;
+        list-style: none;
+        padding-top: 20px;
+      }
+
+      ul.postsbycategory>.featured-news {
+        float: left;
+      }
+
+      li.featured-news {
+        display: inline-block;
+        width: 48%;
+        text-align: center;
+        box-shadow: 0px 4px 6px -4px rgba(35, 39, 52, 0.12), 0px 8px 8px -4px rgba(35, 39, 52, 0.08);
+        transition: box-shadow 0.5s;
+        background-color: #FDFAF7;
+        white-space: normal !important;
+        text-align: left !important;
+      }
+
+      li.featured-news:nth-child(2) {
+        margin-left: 4%;
+      }
+
+      li.featured-news img {
+        float: left;
+        height: 250px !important;
+        width: 160px;
+        margin-right: 10px;
+      }
+
+      li.featured-news a {
+        text-decoration: none;
+        font-family: 'Botera TFE';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 24px;
+        line-height: 29px;
+        color: #232734;
+      }
+
+      li.featured-news>div.news-title-container {
+        padding: 10px;
+      }
+
+      li.featured-news>div.news-content-container>p {
+        padding: 10px;
+        font-family: 'Poppins';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 24px;
+
+        /* gris */
+
+        color: #7680A3;
+      }
+
       .featured-products {
         padding-left: 30px;
       }
@@ -409,6 +552,10 @@ function woo_featured_products()
         align-items: flex-start;
       }
 
+      .right-side h1 a {
+        color: #232734;
+      }
+
       .right-side>.our-restaurant {
         font-family: 'Poppins';
         font-style: normal;
@@ -473,14 +620,12 @@ function woo_featured_products()
         text-decoration: none;
         text-align: center;
       }
-
-      section.single-product section.content section.product-details section.add-cart a {}
     </style>
     <section class="titled-section">
-      <h1 class="title">
+      <h1 class="title hoverable-title">
         Vinos<span class="mobile-br"><br></span> Destacados
-        <span class="section-more"><a href="catalogo/">Ver catálogo</a> </span>
       </h1>
+      <span class="section-more"><a href="catalogo/">Ver catálogo</a> </span>
       <div class="section-body">
         <div class="featured-products">
           <?php $args = array(
@@ -501,15 +646,15 @@ function woo_featured_products()
     <section class="titled-section coming-soon">
       <div class="section-body">
         <div class="left-side">
-          <img src="/wp-content/uploads/2022/05/restaurante-3.png" style="width: 50%;">
-          <img src="/wp-content/uploads/2022/05/restaurante-4.png" style="width: 50%;">
+          <img src="<?php bloginfo('template_url'); ?>/img/homepage/restaurante-3.png" style="width: 50%;">
+          <img src="<?php bloginfo('template_url'); ?>/img/homepage/restaurante-4.png" style="width: 50%;">
         </div>
         <div class="right-side">
           <div>
             <h4 class="greyed-out">Próximamente</h4>
           </div>
           <div>
-            <h1><a href="restaurante-eventos/" style="text-decoration: none;color:#232734;">Nuestro Restaurante</a></h1>
+            <h1><a class="hoverable-title" href="restaurante-eventos/" style="text-decoration: none;">Nuestro Restaurante</a></h1>
           </div>
           <div class="our-restaurant">
             <p>Ubicado en El Sauzal, Ensenada.
@@ -527,7 +672,7 @@ function woo_featured_products()
             <h4 class="greyed-out">Nuestro Restaurante</h4>
           </div>
           <div>
-            <h2>Dulce Vida</h2>
+            <h2 class="hoverable-title">Dulce Vida</h2>
           </div>
           <p class="our-restaurant">
             Ven y disfruta de nuestro asador campestre a cargo de la chef Dulce López.
@@ -537,13 +682,24 @@ function woo_featured_products()
             <a href="restaurante-eventos/"><span>Reserva</span></a>
           </section>
           <div class="dulce-vida-imgs" style="margin-top:15px;">
-            <img src="/wp-content/uploads/2022/05/restaurante-1.png" style="width: 50%;">
-            <img src="/wp-content/uploads/2022/05/restaurante-2.png" style="width: 50%;">
+            <img src="<?php bloginfo('template_url'); ?>/img/homepage/restaurante-1.png" style="width: 50%;">
+            <img src="<?php bloginfo('template_url'); ?>/img/homepage/restaurante-2.png" style="width: 50%;">
           </div>
         </div>
       </div>
     </section>
-  </section>
+    <section class="titled-section">
+      <h1 class="title hoverable-title">
+        Noticias
+      </h1>
+      <span class="section-more"><a href="noticias/">Ver todo</a> </span>
+      <div class="section-body" style="max-width:100%;">
+        <?php
+        echo do_shortcode('[categoryposts]');
+        ?>
+      </div>
+    </section>
+  </section> <!-- CONTENT END -->
 
 </section>
 <style>
@@ -565,6 +721,7 @@ function woo_featured_products()
     font-size: 40px;
     line-height: 48px;
     position: relative;
+    width: fit-content;
   }
 
   .titled-section .section-more a {
@@ -679,7 +836,8 @@ function woo_featured_products()
     transition: box-shadow 0.5s;
   }
 
-  .home-card:hover {
+  .home-card:hover,
+  li.featured-news:hover {
     box-shadow: 0px 8px 28px -6px rgba(35, 39, 52, 0.12), 0px 18px 88px -4px rgba(35, 39, 52, 0.14);
   }
 
